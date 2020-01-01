7066abfb2be48e9765969f46dfdec092dd5e3eb7
yjqiang <yjqiang1@gmail.com>
2020-01-01 17:13:26 +0800

    fix

---
+++ conf/t_dict.py
@@ -0,0 +1,11 @@
+original = "t_dict.pkl"
+destination = "t_dict.pkl"
+
+
+outsize = 0
+with open(original, 'rb') as infile:
+    content = infile.read()
+with open(destination, 'wb') as output:
+    for line in content.splitlines():
+        outsize += len(line) + 1
+        output.write(line + str.encode('\n'))
